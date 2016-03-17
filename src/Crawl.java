33e3d39e191765e92038d202f167bd76b995f842
cabbagenom <calumcalder@hotmail.co.uk>
2016-03-17 12:05:05 +0000

    Allowed for plurality and general tidy up + docstrings

--- src/Crawl.java
+++ src/Crawl.java
@@ -13,9 +13,10 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Created by Stefan on 25/02/2016.
+ * Created by Calum and Stefan.
  */
 public class Crawl {
+	
     public Crawl() {
 	try {
 		state = connectDataBase().createStatement(); //create a database connection
@@ -36,6 +37,9 @@ public class Crawl {
     static final double EVENNESS_THRESHOLD = 0.3;
 
     public Connection connectDataBase() {
+	/**
+	 * Initialises the connection to the database.
+	 **/
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -53,17 +57,13 @@ public class Crawl {
         return con;
     }
 
-
     private double calculateEvenness(ArrayList<Integer> species) {
-
-        LinkedHashMap<Integer,Integer>temp = new LinkedHashMap<Integer,Integer>();//get frequencies
-        for(int j = 0; j <  species.size(); j++) {
-            if(temp.get(species.get(j)) == null){
-                temp.put(species.get(j),1);
-            }
-            else if(temp.get(species.get(j)) != null){
-                temp.put(species.get(j),temp.get(species.get(j))+1);
-            }
+	/**
+	 * Calculates the evenness of a given set of votes.
+	 **/
+        DefaultHashMap<Integer,Integer>temp = new DefaultHashMap<Integer,Integer>();//get frequencies
+        for(Integer speciesID : species) {
+            temp.put(speciesID, temp.getOrDefault(speciesID, 0) + 1);
         }
 
         if (temp.size() == 1) //all agree avoid processing.
@@ -82,38 +82,6 @@ public class Crawl {
 
     }
 
-    public Integer readIndex(String filename)
-    {
-        Path file = Paths.get(filename);
-        Charset charset = Charset.forName("US-ASCII");
-        try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                return Integer.parseInt(line);
-            }
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
-        }
-        return 0;
-    }
-
-    public Integer writeIndex(String filename,Integer Index) //index- where the algorithm has currently processed.
-    {
-        // Convert the string to a
-        // byte array.
-        String s = Index.toString();
-        byte data[] = s.getBytes();
-        Path p = Paths.get(filename);
-
-        try (OutputStream out = new BufferedOutputStream(
-                Files.newOutputStream(p, CREATE, WRITE))) {
-            out.write(data, 0, data.length);
-        } catch (IOException x) {
-            System.err.println(x);
-        }
-        return 0;
-    }
-    
     public Integer getMedianPlurality(Integer photo_id) throws SQLException {
 	/**
 	 * Gets the median number of different species people have classified as being in a given photo.
@@ -123,43 +91,14 @@ public class Crawl {
 	photo_id.toString() + " " +
 	"GROUP BY `person_id`, `species`) AS GroupedVotes " +
 	"GROUP BY `person_id` ORDER BY count;";
+	
 	ResultSet countsResultSet = getMedianState.executeQuery(query);
 	ArrayList<Integer> counts = new ArrayList<Integer>();
 	
 	while (countsResultSet.next())
 		counts.add(countsResultSet.getInt("count"));
 	
-	return counts.get((int) Math.ceil(counts.size()/2.0));
-    }
-
-    public Integer getRowInTable(ResultSet animalInstance,Statement state) //convert animal_id 'index' to row number so result set can be changed to the correct
-    {
-        Integer index = readIndex("index.txt");
-        Integer row = 0;
-        String query = "SELECT COUNT(*) AS row FROM MammalWeb.Animal WHERE animal_id  <= (SELECT animal_id FROM  MammalWeb.Animal WHERE animal_id = '"+index+"')";
-
-                try {
-                    animalInstance = state.executeQuery(query);
-                    if(animalInstance.next()){
-                        row = Integer.parseInt(animalInstance.getString("row"));
-                        System.out.println("Current Row at: " + animalInstance.getString("row"));
-                    }
-                }
-                catch(SQLException e) {
-                    return 0;
-                }
-        return row;
-    }
-
-    public boolean setClassifiedFlag(Integer animal_id, Statement state) {
-        try {
-            String queryString = "UPDATE  `MammalWeb`.`Animal` SET  `classified` =  '1' WHERE  `Animal`.`animal_id` = " + animal_id.toString() + ";";
-            state.executeUpdate(queryString);
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+	return counts.get((int) Math.ceil((counts.size() - 1)/2.0));
     }
 
     Statement state;
@@ -171,12 +110,18 @@ public class Crawl {
     Integer setRow = 0;
 
     public void updatePhotos(Set<Integer> photosToUpdate) throws SQLException {
+	 /**
+	  * Updates a set of photos.
+	  */
          for (Integer photo_id : photosToUpdate) {
              updatePhoto(photo_id);
          }
     }
 
     public boolean consecutiveBlanks(ArrayList<Integer> voteSpecies) {
+	/**
+	 * Calculates if there are 5 consecutive blank votes.
+	 **/
         if (voteSpecies.size() < 5)
             return false;
 
@@ -190,49 +135,48 @@ public class Crawl {
             if (consecutive >= 5)
                 return true;
         }
-
+	
         return false;
     }
 
-    public Integer consensus(ArrayList<Integer> voteSpecies){
-        HashMap<Integer,Integer>speciesCount = new HashMap<Integer,Integer>();
-        for(Integer speciesID : voteSpecies)
-            speciesCount.put(
-                    speciesID,
-                    speciesCount.getOrDefault(speciesID, 0) + 1
-            );
-        Integer consensusID = -1;
-        Integer countMax = 0;
-        for(Integer key : speciesCount.keySet()) {
-            Integer votes = speciesCount.get(key);
-            if (votes > countMax && votes >= 10) {
-                consensusID = key;
-                countMax = votes;
-            }
-        }
-        return consensusID;
+    public ArrayList<Integer> consensus(ArrayList<Integer> voteSpecies, Integer count){
+	/**
+	 * Calculates the consensus of votes, taking in to account the minimum 10 votes requirement.
+	 **/
+        DefaultHashMap<Integer,Integer> speciesCount = new DefaultHashMap<Integer,Integer>();
+        Integer cur = 0;
+        for(Integer speciesID : voteSpecies) {
+                speciesCount.put(speciesID, speciesCount.getOrDefault(speciesID, 0) + 1);
+	}
+	ArrayList<Integer> res = getConsensusFromSpeciesCount(speciesCount, count);
+	for (Integer species : res) {
+		if (speciesCount.get(species) < 10) 
+			return new ArrayList<Integer>() {{ this.add(-1); }};
+	}
+	
+	return res;
     }
     
     public ArrayList<Integer> consensusNoMin(ArrayList<Integer> voteSpecies, Integer count) {
-	HashMap<Integer,Integer>speciesCount = new HashMap<Integer,Integer>();
-        for(Integer speciesID : voteSpecies)
-            speciesCount.put(
-                    speciesID,
-                    speciesCount.getOrDefault(speciesID, 0) + 1
-            );
+	/**
+	 * Calculates the consensus of votes, ignoring the minimum 10 votes requirement.
+	 **/
+	DefaultHashMap<Integer,Integer>speciesCount = new DefaultHashMap<Integer,Integer>();
+	Integer cur;
+        for(Integer speciesID : voteSpecies) {
+                speciesCount.put(speciesID, speciesCount.getOrDefault(speciesID, 0) + 1);
+	}
 	
-	return getConsensusFromSpeciesCount(speciesCount);
+	return getConsensusFromSpeciesCount(speciesCount, count);
     }
     
     public ArrayList<Integer> getConsensusFromSpeciesCount(HashMap<Integer, Integer> speciesCount, Integer count) {
-	// TODO: Unit test this
-	ArrayList<Integer> ordered = new ArrayList<Integer>(speciesCount.keys());
+	/**
+	 * Calculates the top <count> classifications from a count of each species vote.
+	 **/
+	ArrayList<Integer> ordered = new ArrayList<Integer>(speciesCount.keySet());
 	
-	Collections.sort(ordered, new Comparator<Integer>() {
-		public int compare(Integer i1, Integer i2) {
-			return speciesCount.get(i2) - speciesCount.get(i1);
-		}	
-	});
+	Collections.sort(ordered, new CountComparator(speciesCount));
 	ArrayList<Integer> res = new ArrayList<Integer>();
 	for (int i = 0; i < count; i++) {
 		res.add(ordered.get(i));
@@ -242,40 +186,63 @@ public class Crawl {
     }
 
     public void updatePhoto(Integer photo_id) throws SQLException {
+	/**
+	 * Updates a given photo.
+	 * Calculates consecutive blanks, consensus or evenness for the photo and updates database accordingly.
+	 **/
         ArrayList<Integer> species = getPhotoVotes(photo_id);
 	Integer speciesCount = getMedianPlurality(photo_id);
+	ArrayList<Integer> option_ids;
 	
         if (consecutiveBlanks(species)) {
-            classifyImage(ID_NONE, photo_id);
-	    System.out.println("photo_id: " + photo_id + "\tClassified as blank with 5 consecutive votes.");
+	    option_ids = new ArrayList<Integer>() {{ this.add(ID_NONE); }};
+            classifyImage(option_ids, photo_id);
 	    return;
         }
-	Integer option_id = consensus(species);
-        if (!option_id.equals(new Integer(-1))) {
-            classifyImage(option_id, photo_id);
-	    System.out.println("photo_id: " + photo_id + "\tClassified with 10 votes as: " + option_id);
+	
+	option_ids = consensus(species, speciesCount);
+        if (!option_ids.contains(new Integer(-1))) {
+            classifyImage(option_ids, photo_id);
 	    return;
         }
+	
+	option_ids = consensusNoMin(species, speciesCount);
         if (species.size() >= 25) {
 	    double evenness = calculateEvenness(species);
-	    option_id = consensusNoMin(species, speciesCount);
-	    classifyImage(option_id, photo_id);
+	    setEvenness(evenness, photo_id);
+	    return;
         }
 	System.out.println("photo_id: " + photo_id + "\tdoes not have enough votes to be classified");
     }
     
-    public void classifyImage(Integer option_id, Integer photo_id) {
+    public void setEvenness(Double evenness, Integer photo_id) throws SQLException {
+	    /**
+	     * Sets the evenness of a given photo
+	     **/
+	    String updateQuery = "UPDATE `MammalWeb`.`Photo` SET `evenness` = " + evenness.toString() + "WHERE `photo_id` = " + photo_id.toString() + ";";
+	    updateClassifiedState.executeUpdate(updateQuery);
+    }
+    
+    public void classifyImage(ArrayList<Integer> option_ids, Integer photo_id) {
+	/**
+	 * Classifies a given image with a set of classifications
+	 **/
 	try {
-		String updateQuery = "UPDATE MammalWeb.Photo" + 
-				    " SET classification_id=" + option_id.toString() +
-				    " WHERE photo_id=" + photo_id.toString() + ";";
-		updateClassifiedState.executeUpdate(updateQuery);
+		for (Integer option_id : option_ids) {
+			String blanks = option_id == ID_NONE ? "1" : "0"; 
+			String updateQuery = "INSERT INTO `MammalWeb`.`XClassification`(`person_id`, `photo_id`, `humans`, `nothing`, `species`)" +
+					     "VALUES (-1," + photo_id.toString() + ",0," + blanks + "," + option_id.toString() + ")";
+			updateClassifiedState.executeUpdate(updateQuery);
+		}
 	} catch (Exception e) {
 		e.printStackTrace();
 	}
     }
 
     public ArrayList<Integer> getPhotoVotes(Integer photo_id) throws SQLException {
+	/**
+	 * Gets a list of votes on a given photo
+	 **/
         ArrayList<Integer> species = new ArrayList<Integer>();
         String query = "SELECT species FROM MammalWeb.Animal WHERE photo_id = " + photo_id.toString() + ";";
         ResultSet relatedPhotos = state.executeQuery(query);
@@ -287,26 +254,23 @@ public class Crawl {
     }
 
     public boolean classifyImages() {
-        
-
-        String getAnimalInstance = "SELECT animal_id, photo_id FROM MammalWeb.Animal WHERE classified = 0 ORDER BY animal_id"; //get photo in ascending order
+	// Get unclassified photos in ascending order
+        String getAnimalInstance = "SELECT animal_id, photo_id FROM MammalWeb.Animal WHERE `photo_id` NOT IN (SELECT `photo_id` FROM MammalWeb.XClassification) ORDER BY animal_id"; 
         String getPhotos = null;
         try {
             ResultSet animalInstance = null;
             ResultSet countPhotoInstances = null;
             ResultSet getPhotoInstances = null;
 
-            //Integer row = getRowInTable(animalInstance, InsertState);
+	    // Generate set of photos to be updated
             animalInstance = state.executeQuery(getAnimalInstance);
             LinkedHashSet<Integer> photosToUpdate = new LinkedHashSet<Integer>();
-            //animalInstance.absolute(row);
             while (animalInstance.next()) {
                 photosToUpdate.add(animalInstance.getInt("photo_id"));
-                lastID = Integer.parseInt(animalInstance.getString("animal_id"));
             }
-	    System.out.println("Calculated photos to update");
+	    // Update all photos in set
             updatePhotos(photosToUpdate);
-            writeIndex("index.txt", lastID);
+	    
         }catch(Exception e){
 	    e.printStackTrace();
             return false;
@@ -319,15 +283,39 @@ public class Crawl {
     public static void main(String[] args)
     {
         Crawl crawl = new Crawl();
-	try {
-		System.out.println(crawl.getMedianPlurality(200));
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-        //crawl.classifyImages();
-
-        return;
+        try {
+//        	System.out.println(crawl.getMedianPlurality(10000));
+	        //crawl.updatePhoto(160);
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        crawl.classifyImages();
     }
 
 }
 
+class CountComparator implements Comparator<Integer> {
+	private HashMap<Integer, Integer> speciesCount;
+	
+	public CountComparator(HashMap<Integer,Integer> speciesCount) {
+		this.speciesCount = speciesCount;
+	}
+	
+	public int compare(Integer i1, Integer i2) {
+		return speciesCount.get(i2) - speciesCount.get(i1);
+	}
+	
+}
+
+class DefaultHashMap<K, V> extends HashMap<K, V> {
+	
+	public V getOrDefault(K key, V def) {
+		V get = this.get(key);
+		
+		if (get == null)
+			return def;
+		else
+			return get;
+	}
+	
+}
