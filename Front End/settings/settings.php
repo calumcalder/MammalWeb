80a231d85cdaf5871849071c73f04ca1fad849c1
DU8901 <gaar321@live.co.uk>
2016-04-26 11:29:34 +0100

    Update settings

--- Front End/settings/settings.php
+++ Front End/settings/settings.php
@@ -14,6 +14,7 @@ $_POST["runFrequency"] = clean($_POST["runFrequency"]);
 $_POST["minAge"] = clean($_POST["minAge"]);
 $_POST["evennessThres"] = clean($_POST["evennessThres"]);
 $_POST["maxPhoto"] = clean($_POST["maxPhoto"]);
+$_POST["minGender"] = clean($_POST["minGender"]);
 
 if(isset($_POST["consensusCount"])){
 	$_POST["consensusCount"] = clean($_POST["consensusCount"]);
@@ -23,6 +24,7 @@ if(isset($_POST["consensusCount"])){
 	$resultSettings = $sqlConnection->query("UPDATE CrawlerSettings SET setting_value=".$_POST["minAge"]." WHERE setting_id='4'");
 	$resultSettings = $sqlConnection->query("UPDATE CrawlerSettings SET setting_value=".$_POST["evennessThres"]." WHERE setting_id='5'");
 	$resultSettings = $sqlConnection->query("UPDATE CrawlerSettings SET setting_value=".$_POST["maxPhoto"]." WHERE setting_id='6'");
+	$resultSettings = $sqlConnection->query("UPDATE CrawlerSettings SET setting_value=".$_POST["minGender"]." WHERE setting_id='7'");
 	echo($resultSettings);
 }
 
